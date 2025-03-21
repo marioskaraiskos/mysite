726f3eb34a76fa4f8b546d6c56c25e99d154d9c2
marioskaraiskos <marioskaraiskosbusiness@gmail.com>
2025-03-21 19:50:13 +0200

    adding the 2 projects in the Projects section

--- src/Projects.js
+++ src/Projects.js
@@ -1,13 +1,28 @@
-
+import todoBoard from './img/todo board.PNG';
+import stopwatch from './img/stopwatch.PNG';
+import './Projects.css'; // Ensure this CSS file exists
 
 function Projects() {
     return (
         <div>
-    <h1>Here are my projects</h1>
-    
-    </div>
-);
-}
+           <ul className="projects-list">
+    <li>
+        <a href="https://marioskaraiskos.github.io/stopwatch/" target="_blank" rel="noopener noreferrer">
+            My Stopwatch App created with React
+            <img src={stopwatch} className="card-img-top" alt="Stopwatch" />
+        </a>
+    </li>
 
+    <li>
+        <a href="https://marioskaraiskos.github.io/to-do-board/" target="_blank" rel="noopener noreferrer">
+            My To-Do-Board  App created with React
+            <img src={todoBoard} className="card-img-top" alt="Stopwatch" />
+        </a>
+    </li>
+    
+</ul>
+        </div>
+    );
+}
 
 export default Projects;
