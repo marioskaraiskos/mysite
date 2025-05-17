26031a922ff9a7e9fd13846fa753efc496a06d1f
marioskaraiskos <marioskaraiskosbusiness@gmail.com>
2025-05-17 18:16:27 +0300

    making the project viable in production

--- backend/server.js
+++ backend/server.js
@@ -61,6 +61,7 @@ app.post('/register', async (req, res) => {
 */
 
 // Start the server
-app.listen(5000, () => {
-  console.log('Server running on port 5000');
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
 });
